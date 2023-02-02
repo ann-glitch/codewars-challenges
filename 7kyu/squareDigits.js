b7b285ca2d56ebab247c49f007173229e432794a
ann <dorisann.me@gmail.com>
2023-02-02 12:48:19 +0000

    sqaureDigits commit

---
+++ 7kyu/squareDigits.js
@@ -0,0 +1,12 @@
+/* 
+Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81) */
+
+function squareDigits(num) {
+  let arr = Array.from(String(num), Number); // split the numbers into an array
+  let result = arr.map((el) => el ** 2).join(""); // square each number and concatenate them.
+  return parseInt(result); // convert the input to a number.
+}
+
+console.log(squareDigits(3212));
---
+++ README.md
@@ -0,0 +1,4 @@
+# Daily Codewars Js Challenges
+
+Here you can find a collection of my level-based CodeWars solutions (kyus).
+One completed challenge (kata) is represented by one.js file.
