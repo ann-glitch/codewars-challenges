52d845831f9489fec38ba83b034398402e6a0c28
ann <dorisann.me@gmail.com>
2023-02-05 22:57:25 +0000

    returnToSanity solution

---
+++ 8kyu/returnToSanity.js
@@ -0,0 +1,19 @@
+/* 
+ This function should return an object,
+ but it's not doing what's intended. What's wrong?
+
+ function mystery() {
+  var results =
+    {sanity: 'Hello'};
+  return
+    results;
+}
+
+*/
+
+function mystery() {
+  var results = { sanity: "Hello" };
+  return results; // 'results' was supposed to be on the same line with 'return'.
+}
+
+console.log(mystery());
