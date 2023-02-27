e28cbd25a11a3800a614190ce58f99a0ad0fc968
ann <dorisann.me@gmail.com>
2023-02-27 19:03:09 +0000

    string ending solution

--- 7kyu/squareDigits.js
+++ 7kyu/squareDigits.js
@@ -5,10 +5,19 @@ For example, if we run 9119 through the function, 811181 will come out, because 
 
 and 1 squared up is 1. (81-1-1-81) */
 
-function squareDigits(num) {
-  let arr = Array.from(String(num), Number); // split the numbers into an array
-  let result = arr.map((el) => el ** 2).join(""); // square each number and concatenate them.
-  return parseInt(result); // convert the input to a number.
-}
+// function squareDigits(num) {
+//   let arr = Array.from(String(num), Number); // split the numbers into an array
+//   let result = arr.map((el) => el ** 2).join(""); // square each number and concatenate them.
+//   return parseInt(result); // convert the input to a number.
+// }
 
-console.log(squareDigits(3212));
+// function squareDigits(num) {
+//   let answer = String(num)
+//     .split("")
+//     .map((el) => el ** 2)
+//     .join("");
+
+//   return parseInt(answer);
+// }
+
+// console.log(squareDigits(3212));
